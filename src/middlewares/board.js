1f8623a4d781b3f555b71d9f3f9b3fadf024b04b
SpitaI <alex.asm.1997@gmail.com>
2021-05-28 14:57:56 +0300

    feat: write JSDoc for middlewares

--- src/middlewares/board.js
+++ src/middlewares/board.js
@@ -1,6 +1,13 @@
 import * as boardService from '../services/board.js';
 import { BOARD_ERRORS } from '../constants/errors.js';
 
+/**
+ * Middleware to check for existance of Board with given id
+ * @param {import('express').Request} req - Request object
+ * @param {import('express').Response} res - Response object
+ * @param {import('express').NextFunction} next - Next function
+ * @returns {Promise<void | import('express').Response>}
+ */
 export const checkBoard = async (req, res, next) => {
   const { boardId } = req.params;
   const board = await boardService.getById(boardId);
--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,8 +1,18 @@
+/**
+ * Get details of validation errors
+ * @param {{ path: string, message: string }[]} errors - Array containing Joi validation errors
+ * @returns {{ code: 400, errors: { path: string, message: string }[] }} Object with details of validation errors
+ */
 const getErrorDetails = errors => ({
   code: 400,
   errors: errors.map(({ path, message }) => ({ path, message })),
 });
 
+/**
+ * Create validation middleware for given Joi schema
+ * @param {import('joi').Schema} schema - Joi schema for request body validation
+ * @returns {(req: import('express').Request, res: import('express').Response, next: import('express').NextFunction) => void} Validation middleware
+ */
 export const getValidationMiddleware = schema => async (req, res, next) => {
   const { error } = schema.validate(req.body);
 
