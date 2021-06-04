246a8cd9cab4cc2ccffafb0cb43ce91548f557c5
SpitaI <alex.asm.1997@gmail.com>
2021-06-05 00:46:44 +0300

    feat: add logger middlewares

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,7 @@
+import { Request, Response, NextFunction } from 'express';
 import fs, { WriteStream } from 'fs';
 import path from 'path';
+import chalk from 'chalk';
 
 import config from '../common/config';
 import { DEFAULT_LOG_DIRNAME } from '../constants/logger';
@@ -41,6 +43,18 @@ class ExpressLogger {
     return [logPath, errorPath];
   }
 
+  private static formatStatusCode(statusCode: number) {
+    const code = `${statusCode}`;
+
+    if (code.startsWith('2')) return chalk.green(code);
+    if (code.startsWith('3')) return chalk.cyan(code);
+    return chalk.red(code);
+  }
+
+  private static clearColor(str: string): string {
+    return str.replace(/\[\d+m/g, '');
+  }
+
   constructor(opts?: LoggerOptions) {
     const { dirname, filename, errorFilename } = opts || {};
 
@@ -57,14 +71,60 @@ class ExpressLogger {
     const msg = `${message.join(' ')}\n`;
 
     process.stdout.write(msg);
-    this.logStream?.write(msg);
+    this.logStream?.write(ExpressLogger.clearColor(msg));
   }
 
   error(...errorMessage: string[]): void {
     const errMsg = `${errorMessage.join(' ')}\n`;
 
     process.stderr.write(errMsg);
-    this.errorLogStream?.write(errMsg);
+    this.errorLogStream?.write(ExpressLogger.clearColor(errMsg));
+  }
+
+  getLogMiddleware() {
+    return async (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ): Promise<void> => {
+      const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+      const queryParams = JSON.stringify(req.query);
+      const body = JSON.stringify(req.body);
+      const date = new Date().toISOString();
+
+      const timeStart = Date.now();
+
+      res.on('finish', () => {
+        const timeDelta = Date.now() - timeStart;
+        this.log(
+          `[${chalk.gray(date)}]`,
+          `${chalk.greenBright(req.method)}`,
+          `${url}`,
+          `${chalk.magenta('QUERY')}: ${queryParams}`,
+          `${chalk.magenta('BODY')}: ${body}`,
+          ExpressLogger.formatStatusCode(res.statusCode),
+          `${timeDelta}ms`
+        );
+      });
+
+      next();
+    };
+  }
+
+  getErrorMiddleware() {
+    return async (
+      err: Error,
+      _req: Request,
+      _res: Response,
+      next: NextFunction
+    ): Promise<void> => {
+      const date = new Date().toISOString();
+      const errorMessage = `[${chalk.gray(date)}] ${chalk.red('ERROR')} ${
+        err.message
+      }`;
+      this.error(errorMessage, `\n${err.stack}\n`, '-'.repeat(100));
+      next(err);
+    };
   }
 }
 
