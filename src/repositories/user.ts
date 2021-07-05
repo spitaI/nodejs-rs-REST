63117a52c1c4a7820a879976df898d2327a51c30
SpitaI <alex.asm.1997@gmail.com>
2021-07-05 18:26:43 +0300

    feat: nest repositories

--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -1,26 +1,13 @@
-import User, { IUser, UserResponse } from '../models/user';
-import { getEntityDAO } from '../utils/database';
+import { Injectable } from '@nestjs/common';
+import { Connection } from 'typeorm';
 
-const userDAO = getEntityDAO<IUser>(User);
+import User, { IUser } from '../models/user';
+import { EntityDAO } from '../utils/database';
 
-export const getAll = async (): Promise<UserResponse[]> => userDAO.getAll();
-
-export const getById = async (id: string): Promise<UserResponse | null> =>
-  userDAO.find({ id });
-
-export const getByUsername = async (username: string): Promise<IUser | null> =>
-  userDAO.find({
-    where: { login: username },
-    select: ['id', 'name', 'login', 'password'],
-  });
-
-export const create = async (user: IUser): Promise<UserResponse | null> =>
-  User.toResponse(await userDAO.create(user));
-
-export const updateById = async (
-  id: string,
-  userData: Partial<IUser>
-): Promise<UserResponse | null> => userDAO.update({ id }, userData);
-
-export const deleteById = async (id: string): Promise<boolean> =>
-  userDAO.remove({ id });
+@Injectable()
+export class UserRepository extends EntityDAO<IUser> {
+  constructor(connection: Connection) {
+    const userRepo = connection.getRepository(User);
+    super(userRepo);
+  }
+}
